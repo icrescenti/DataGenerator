db8c4fea896ee1e9569cd10cd3c17bd746468474
icrescenti <icrescenti@terkstudios.com>
2021-08-03 19:54:55 +0200

    SQL GENERATION, AUTOINCREMENT, RANGE AND STX FIX

--- generator.py
+++ generator.py
@@ -2,30 +2,62 @@ import sys
 import json
 import random
 
-if len(sys.argv) <= 1:
-    sys.exit()
+f = None
 
-def convertToFormat(label, xtype, value):
-    if(sys.argv[1] == "sql"):
-        print("(Sample) INSERT INTO " + label + " VALUES (" + value + ");")
-
-f = open('structure.json',)
+if len(sys.argv) > 1:
+    f = open(sys.argv[1],)
+else:
+    f = open('structure.json',)
 
 data = json.load(f)
-  
-for item in data:
-    valuesFStream = ""
-    xtype = 0
-    
-    if (item['value'] == "first_name"):
-        valuesFStream = open('data/first_names.json',)
-        xtype = 1
-    elif (item['value'] == "middle_name"):
-        valuesFStream = open('data/middle-names.json',)
-        xtype = 1
-    
-    valuesArray = json.load(valuesFStream)
-    convertToFormat(item['label'], xtype, valuesArray[random.randrange(1,len(valuesArray))])
-    valuesFStream.close()
+
+def convertToFormat(headers, values):
+    if(data['format'] == "sql"):
+        print("INSERT INTO " + data['tablename'] + 
+        " (" +
+            (",".join(headers)) + 
+        ") VALUES (" + 
+            (",".join(values))
+        + ");")
+    elif(data['format'] == "csv"):
+        print("(TO-DO)")
+
+for _ in range(data['quanitity']):
+    headers = []
+    values = []
+
+    for item in data['fields']:
+        headers.append("\'" + item['label'] + "\'")
+
+        valuesFStream = None
+        xtype = 0
+        value = 0
+        
+        if (item['value'] == "integer"):
+            xtype = 0
+        elif (item['value'] == "first_name"):
+            valuesFStream = open('data/first_names.json',)
+            xtype = 1
+        elif (item['value'] == "middle_name"):
+            valuesFStream = open('data/middle-names.json',)
+            xtype = 1
+        
+        if (xtype == 0):
+            try:
+                value = item['autoIncrementFrom'] + _
+            except KeyError:
+                try:
+                    xrange = item['range']
+                    value = random.randrange(xrange[0],xrange[1])
+                except KeyError:
+                    value = random.randrange(0,100)
+
+        elif (xtype == 1):
+            valuesArray = json.load(valuesFStream)
+            value = "\'" + valuesArray[random.randrange(0,len(valuesArray))] + "\'"
+            valuesFStream.close()
+
+        values.append(str(value))
+    convertToFormat(headers, values)
   
 f.close()
