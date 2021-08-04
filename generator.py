efb977b74c00f81920567b27913d75b30cb1aacf
icrescenti <icrescenti@terkstudios.com>
2021-08-04 14:39:36 +0200

    dates, save to file and error counter

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import random
+import time
 
 f = None
 
@@ -11,16 +12,33 @@ else:
 
 data = json.load(f)
 
+errors = 0
+
+def str_time_prop(start, end, time_format, prop):
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+    ptime = stime + prop * (etime - stime)
+    return time.strftime(time_format, time.localtime(ptime))
+ 
+def random_date(start, end, xformat, prop):
+    return str_time_prop(start, end, xformat, prop)
+
 def convertToFormat(headers, values):
     if(data['format'] == "sql"):
-        print("INSERT INTO " + data['tablename'] + 
+        writeToFile("INSERT INTO " + data['tablename'] + 
         " (" +
             (",".join(headers)) + 
         ") VALUES (" + 
             (",".join(values))
-        + ");")
+        + ");\n")
     elif(data['format'] == "csv"):
         print("(TO-DO)")
+
+def writeToFile(value):
+    if(data['format'] == "sql"):
+        f = open('generated.sql','a')
+        f.write(value)
+        f.close()
 
 for _ in range(data['quanitity']):
     headers = []
@@ -34,15 +52,6 @@ for _ in range(data['quanitity']):
         value = 0
         
         if (item['value'] == "integer"):
-            xtype = 0
-        elif (item['value'] == "first_name"):
-            valuesFStream = open('data/first_names.json',)
-            xtype = 1
-        elif (item['value'] == "middle_name"):
-            valuesFStream = open('data/middle-names.json',)
-            xtype = 1
-        
-        if (xtype == 0):
             try:
                 value = item['autoIncrementFrom'] + _
             except KeyError:
@@ -52,12 +61,34 @@ for _ in range(data['quanitity']):
                 except KeyError:
                     value = random.randrange(0,100)
 
-        elif (xtype == 1):
+        elif (item['value'] == "first_name"):
+            valuesFStream = open('data/first_names.json',)
+            xtype = 1
+
+        elif (item['value'] == "middle_name"):
+            valuesFStream = open('data/middle-names.json',)
+            xtype = 1
+
+        elif (item['value'] == "last_name"):
+            valuesFStream = open('data/last-names.json',)
+            xtype = 1
+
+        elif (item['value'] == "date"):
+            try:
+                xrange = item['range']
+                value = "\'" + random_date(xrange[0], xrange[1], item['format'], random.random()) + "\'"
+            except KeyError:
+                print("ERROR: " + item['label'] + " doesen't have range or format attribute")
+                errors += 1
+                value = "\'" + random_date("1/1/1970 00:00:00", "31/12/2099 00:00:00", '%d/%m/%Y %H:%M:%S', random.random()) + "\'"
+            
+        if (xtype == 1):
             valuesArray = json.load(valuesFStream)
             value = "\'" + valuesArray[random.randrange(0,len(valuesArray))] + "\'"
             valuesFStream.close()
 
         values.append(str(value))
     convertToFormat(headers, values)
-  
+
 f.close()
+print("Script ended, information generated with " + str(errors) + " errors")
